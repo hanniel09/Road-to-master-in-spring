3f5c2d998b484a34f31a1e10a8cac0bf8e16fd60
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-05 23:59:10 -0300

    feature: Spring Jpa-hibernate crud save students method

--- spring-boot-hibernate-crud/00-starter-sql-scripts/cruddemo/src/main/java/com/hanniel/cruddemo/CruddemoApplication.java
+++ spring-boot-hibernate-crud/00-starter-sql-scripts/cruddemo/src/main/java/com/hanniel/cruddemo/CruddemoApplication.java
@@ -17,8 +17,24 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 new Student object ...");
+		Student tempStudent1 = new Student("John", "Doe", "John@Doe.com");
+		Student tempStudent2 = new Student("Mary", "Public", "Mary@Public.com");
+		Student tempStudent3 = new Student("Barry", "Allen", "Barry@Allen.com");
+
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
