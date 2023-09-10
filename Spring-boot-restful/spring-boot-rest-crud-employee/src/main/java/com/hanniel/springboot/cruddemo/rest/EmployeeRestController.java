66244aa6755b977e8b905f2378603ecdc7b36960
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-10 13:33:26 -0300

    Feature:Get ALl Employees to in memory database

--- Spring-boot-restful/spring-boot-rest-crud-employee/src/main/java/com/hanniel/springboot/cruddemo/rest/EmployeeRestController.java
+++ Spring-boot-restful/spring-boot-rest-crud-employee/src/main/java/com/hanniel/springboot/cruddemo/rest/EmployeeRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
+    private final EmployeeDAO employeeDAO;
 
     public EmployeeRestController(EmployeeDAO theEmployeeDAO){
             employeeDAO = theEmployeeDAO;
---
+++ Spring-boot-restful/spring-boot-rest-crud-employee/src/main/java/com/hanniel/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.hanniel.springboot.cruddemo.service;
+
+public interface EmployeeService {
+}
