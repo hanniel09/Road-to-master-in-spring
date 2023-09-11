1c8ac5c754fb84f53598fcd913c3b6b9b6ed4abb
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-11 18:11:05 -0300

    Feature: Studies Spring security

---
+++ spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/hanniel/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.hanniel.springboot.cruddemo.dao;
+
+import com.hanniel.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/hanniel/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.hanniel.springboot.cruddemo.service;
+
+import com.hanniel.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
