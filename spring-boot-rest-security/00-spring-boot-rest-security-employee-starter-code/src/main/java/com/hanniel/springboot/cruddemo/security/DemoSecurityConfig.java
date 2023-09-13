911cf5cf4a33beb03e978267a6ddc15d78874acf
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-13 19:29:10 -0300

    Feature: Configure Spring security with sql code.

--- spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/hanniel/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/hanniel/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -19,7 +19,17 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -62,7 +72,7 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-*/
+        }
+    */
 }
 
