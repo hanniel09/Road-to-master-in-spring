d3ac23dfac443135dc10ef75e1ef2b70cd2a2cf3
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-15 18:31:39 -0300

    Feature: Free Passes form update

--- spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/Customer.java
+++ spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.hanniel.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -13,9 +10,21 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName = "";
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "Must be greater than or equal to zero")
     @Max(value = 10, message = "Must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
