4d126c542f4d5f250ec9cb4d9bdda7f8e089019e
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-14 23:03:04 -0300

    Initial commit

---
+++ spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/CustomerController.java
@@ -0,0 +1,16 @@
+package com.hanniel.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+}
