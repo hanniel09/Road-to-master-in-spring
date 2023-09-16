867c0c07803cde1478cea7e089e72ba8f071539d
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-16 10:37:30 -0300

    Feature: freePasse integer validator form update

--- spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/validation/CourseCode.java
+++ spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/validation/CourseCode.java
@@ -1,6 +1,7 @@
 package com.hanniel.springdemo.mvc.validation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,4 +12,12 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
+
+    public String value() default "CODE";
+
+    public String message( ) default "must start with CODE";
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
 }
