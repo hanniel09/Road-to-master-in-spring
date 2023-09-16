99fefef8c7a51706d1573a82f5c17d3f2620db40
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-16 10:59:26 -0300

    Feature: CourseCodeConstraintValidator create

--- spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/validation/CourseCode.java
+++ spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/validation/CourseCode.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = CourseCodeContraintValidator.class)
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
---
+++ spring-boot-spring-mvc/validationdemo/src/main/java/com/hanniel/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,19 @@
+package com.hanniel.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+        return theCode.startsWith(coursePrefix);
+    }
+}
