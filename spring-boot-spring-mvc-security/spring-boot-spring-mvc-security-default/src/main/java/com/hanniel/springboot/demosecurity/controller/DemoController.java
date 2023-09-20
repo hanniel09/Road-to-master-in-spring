4b3100b914aad5736f199b4afe5326dde9c5a748
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-20 15:37:37 -0300

    feature: create a security and systems page.

--- spring-boot-spring-mvc-security/spring-boot-spring-mvc-security-default/src/main/java/com/hanniel/springboot/demosecurity/controller/DemoController.java
+++ spring-boot-spring-mvc-security/spring-boot-spring-mvc-security-default/src/main/java/com/hanniel/springboot/demosecurity/controller/DemoController.java
@@ -16,4 +16,9 @@ public class DemoController {
     public String showLeaders(){
         return "leaders";
     }
+
+    @GetMapping("/systems")
+    public String showSystems(){
+        return "systems";
+    }
 }
