6810e6e7c89fca62cfcfa4cb938ce36a55ca975c
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-21 14:52:21 -0300

    feature: configure Jdbc to UserDetailsManager with custom table.

--- spring-boot-spring-mvc-security/spring-boot-spring-mvc-security-default/src/main/java/com/hanniel/springboot/demosecurity/security/DemoSecurityConfig.java
+++ spring-boot-spring-mvc-security/spring-boot-spring-mvc-security-default/src/main/java/com/hanniel/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -15,7 +15,17 @@ public class DemoSecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -40,30 +50,4 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
-    /*
-        @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-
-     */
 }
