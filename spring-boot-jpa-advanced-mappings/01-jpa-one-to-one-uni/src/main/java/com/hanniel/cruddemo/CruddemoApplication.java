57bc1a1736f2edd55655deac9c09d6dd20d5767a
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-22 22:55:58 -0300

    feature: inserts the dao design pattern.

--- spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/hanniel/cruddemo/CruddemoApplication.java
+++ spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/hanniel/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.hanniel.cruddemo;
 
+import com.hanniel.cruddemo.dao.AppDAO;
+import com.hanniel.cruddemo.entity.Instructor;
+import com.hanniel.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,9 +17,24 @@ public class CruddemoApplication {
 
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			System.out.println("Hello world");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		Instructor tempInstructor =
+				new Instructor("Chad", "Darby", "Chad@Darby.com");
+
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://www.google.com/youtube",
+						"Code and developer"
+				);
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor: " + tempInstructor);
 	}
 }
