dee67cf69e6d5838519eee359fd5222f42a4cd8c
hanniel09 <hannielvieira1227@gmail.com>
2023-09-23 14:59:31 -0300

    feature: find by id.

--- spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/hanniel/cruddemo/CruddemoApplication.java
+++ spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/hanniel/cruddemo/CruddemoApplication.java
@@ -19,8 +19,20 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			createInstructor(appDAO);
+//			createInstructor(appDAO);
+
+			findInstrutor(appDAO);
 		};
+	}
+
+	private void findInstrutor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associate instructorDetail only: " + tempInstructor.getInstructorDetail());
 	}
 
 	private void createInstructor(AppDAO appDAO) {
