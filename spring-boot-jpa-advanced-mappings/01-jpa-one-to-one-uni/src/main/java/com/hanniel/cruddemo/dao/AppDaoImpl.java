b9658c4feb93b47946fa1a72bc7a075b4a1b76ac
hanniel09 <hannielvieira1227@gmail.com>
2023-09-26 17:38:33 -0300

    feature: find insctructor and join fetch.

--- spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/hanniel/cruddemo/dao/AppDaoImpl.java
+++ spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/hanniel/cruddemo/dao/AppDaoImpl.java
@@ -64,4 +64,17 @@ public class AppDaoImpl implements AppDAO{
 
         return query.getResultList();
     }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i "
+                + "JOIN FETCH i.courses"
+                + "where i.id = :data", Instructor.class
+        );
+
+        query.setParameter("data", theId);
+
+        return query.getSingleResult();
+    }
 }
