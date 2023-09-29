e364887768aa09e7a18ae923376e1f2989b90997
hanniel09 <hannielvieira1227@gmail.com>
2023-09-29 15:46:18 -0300

    refactor: membership dao

--- spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-    @Before("execution(public void com.hanniel.aopdemo.dao.AccountDAO.addAccount())")
+    @Before("execution(public void add*())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n========>>> Executing @Before advice on addAccount()");
     }
--- spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/dao/MembershipDAO.java
+++ spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/dao/MembershipDAO.java
@@ -1,5 +1,5 @@
 package com.hanniel.aopdemo.dao;
 
 public interface MembershipDAO {
-    void addAccount();
+    void addSillyMember();
 }
