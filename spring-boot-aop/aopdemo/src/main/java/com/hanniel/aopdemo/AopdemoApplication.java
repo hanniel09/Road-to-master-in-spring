be0da4a183ab164d8d7a21185d29eaf8d3e10da5
hanniel09 <hannielvieira1227@gmail.com>
2023-10-03 14:25:47 -0300

    feature: Join pointers test.

--- spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/AopdemoApplication.java
+++ spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/AopdemoApplication.java
@@ -23,6 +23,9 @@ public class AopdemoApplication {
 
 	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
 
--- spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop/aopdemo/src/main/java/com/hanniel/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,11 @@
 package com.hanniel.aopdemo.aspect;
 
+import com.hanniel.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +16,24 @@ public class MyDemoLoggingAspect {
 //    @Before("execution(public void add*())")
 
     @Before("com.hanniel.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(){
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
         System.out.println("\n========>>> Executing @Before advice on method");
+
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        Object[] args = theJoinPoint.getArgs();
+
+        for (Object tempArgs : args){
+            System.out.println(tempArgs);
+
+            if(tempArgs instanceof Account) {
+                Account theAccount = (Account) tempArgs;
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account name: " + theAccount.getLevel());
+            }
+        }
     }
 
 
